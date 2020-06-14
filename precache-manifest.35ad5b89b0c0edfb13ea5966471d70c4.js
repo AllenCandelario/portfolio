0309a356b0045628a0ad8d51a2c3fdf4383a9ae2
Chrisdale Allen Lau Candelario <chrisallen1394@gmail.com>
2020-06-15 04:12:56 +0800

    Updates

---
+++ precache-manifest.35ad5b89b0c0edfb13ea5966471d70c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd7a6a66d6f8585fae6c9269eefcca3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "29ea8e01225084b61a1c",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "b77ce3bb46781e3d2114",
+    "url": "/portfolio/static/css/main.a18ecb95.chunk.css"
+  },
+  {
+    "revision": "29ea8e01225084b61a1c",
+    "url": "/portfolio/static/js/2.21695f10.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.21695f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b77ce3bb46781e3d2114",
+    "url": "/portfolio/static/js/main.9cb9cfeb.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "c983cf495bc10a3492b0564f7e9a0916",
+    "url": "/portfolio/static/media/Sample School Work.c983cf49.pdf"
+  }
+]);
